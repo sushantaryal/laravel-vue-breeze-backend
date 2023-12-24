80ab60842d8de3ecf75daed2e5ba1d79cc54f0b8
Sushant Aryal <sushant.aryal90@gmail.com>
2023-12-24 22:39:12 +0545

    Redirect to login page on logout

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -48,6 +48,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('admin.login');
+        return redirect()->route('admin.login');
     }
 }
