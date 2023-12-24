cdd3e07d26041cc9a726d70f29112529b1256fd4
Sushant Aryal <sushant.aryal90@gmail.com>
2023-12-24 22:39:35 +0545

    Add csrf token

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,7 @@ class HandleInertiaRequests extends Middleware
                 ...(new Ziggy)->toArray(),
                 'location' => $request->url(),
             ],
+            'csrftoken' => csrf_token(),
         ];
     }
 
